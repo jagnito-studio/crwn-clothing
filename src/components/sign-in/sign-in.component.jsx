ccf85aac1fc58c2ca7084d1f15cf191cd3ae49e1
Jason <jasongame61@gmail.com>
2021-09-03 16:23:36 +1000

    Sign in using email and password

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 import './sign-in.styles.scss';
+
 
 class SignIn extends React.Component{
     constructor(props) {
@@ -17,8 +19,18 @@ class SignIn extends React.Component{
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
+
+        const { email, password } = this.state;
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+
+            // Clear up after successful sign in 
+            this.setState({ email: '', password: '' });
+        } catch (error) {
+            console.error(error);
+        }
 
         this.setState({ email: '', password: '' });
     }
